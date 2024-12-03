d06688e627f9b1c7b2db0641bf821032db711719
Cassandra Carpenter <carpenterclynn@gmail.com>
2024-12-03 16:09:08 -0500

    scene

--- Assets/Scripts/SceneTimer.cs
+++ Assets/Scripts/SceneTimer.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTimer : MonoBehaviour
+
 {
-    // Start is called before the first frame update
+    [Header("Timer Settings")]
+    public float timerDuration = 25f;   
+    public string nextSceneName;        
+
+    private float timeRemaining;
+    private bool timerRunning = false;
+
     void Start()
     {
-        
+        timeRemaining = timerDuration;  
+        timerRunning = true;            
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (timerRunning)
+        {
+            timeRemaining -= Time.deltaTime;  
+
+            if (timeRemaining <= 0)
+            {
+                timerRunning = false;        
+                LoadNextScene();             
+            }
+        }
+    }
+
+  
+    void LoadNextScene()
+    {
+        if (!string.IsNullOrEmpty(nextSceneName)) // Make sure a scene name is set
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Next scene name is not set in the inspector!");
+        }
     }
 }
+
