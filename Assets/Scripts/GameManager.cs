7dedc107315e4057b81e28014b686a82b5171729
Disuko <64217407+disukomusic@users.noreply.github.com>
2024-12-03 16:41:53 -0500

    [Change] Multiple Monitors Play Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,11 @@ using Random = UnityEngine.Random;
 public class GameManager : MonoBehaviour
 {
     public bool isPlaying;
+    public float shipHealth;
     private void Start()
     {
         isPlaying = true;
+        ActivateDisplays();
         StartCoroutine(GameLoop());
     }
 
@@ -25,5 +27,20 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(1f);
             
         }   
+    }
+
+    /// <summary>
+    /// https://docs.unity3d.com/2019.3/Documentation/Manual/MultiDisplay.html
+    /// </summary>
+    void ActivateDisplays()
+    {
+        Debug.Log ("displays connected: " + Display.displays.Length);
+        // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
+        // Check if additional displays are available and activate each.
+    
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
     }
 }
